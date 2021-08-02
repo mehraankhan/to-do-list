835c8a3f0f4c1c1175c9fb02e4a288333dc43bde
mehraankhan <khanmehraan@gmail.com>
2021-08-02 22:15:40 +0200

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -30,6 +30,18 @@ const Form = ({
   };
   return (
     <form>
+         <div>
+        <h1>Testing stuff</h1>
+        <h3>Email</h3>
+        <a href="mailto:someone@yoursite.com">test@gmail.com</a>
+        <hr />
+        <h3>Phone</h3>
+        <a href="tel:0831231231">0831231231</a>
+        <hr />
+        <h3>Google maps</h3>
+        <a href="https://www.google.com/maps/@-33.8913753,18.5151581,15z">Open maps</a>
+        <hr />
+      </div>
       <input
         type="text"
         className="todo-input"
